2ade50b2fecb6a7d7b7923a327b3a2fefaff5131
Alex Boatwright <alex.boatwright@postmates.com>
2016-01-25 17:44:17 -0800

    adding some stuff for testing the stuff with the guy with the hair who breathes air

--- index.js
+++ index.js
@@ -1,223 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DropZone from './drop-zone.jsx';
+import EventTravel from './event-travel.js';
+import ImageHandler from './image-handler.js';
+import DragPreview from './drag-preview.jsx';
+import velocity from 'velocity-animate';
 
 if (process.env.BROWSER) {
-    require('./image-preview-upload');
-}
-
-class ImageHandler {
-    onChange = null;
-    canvas = null;
-
-    minWidth = 0;
-    minHeight = 0;
-
-    zoom = 0;
-    _translate = {
-        x: 0,
-        y: 0
-    };
-    _offset = {
-        x: 0,
-        y: 0
-    };
-
-    constructor(onChange) {
-        this.onChange = onChange;
-        this.canvas = document.createElement('canvas');
-    }
-
-    minSize(w, h) {
-        this.minWidth = w;
-        this.minHeight = h;
-
-        this.hardZoom();
-    }
-
-    draw() {
-        const ctx = this.canvas.getContext('2d'),
-            t = this.hardTranslate();
-
-        if (
-            this.lastZoom === this.zoom &&
-            this.lastTranslate.x === t.x &&
-            this.lastTranslate.y === t.y
-        ) {
-            return;
-        }
-
-        this.lastZoom = this.zoom;
-        this.lastTranslate = t;
-
-        this.canvas.width = this.minWidth;
-        this.canvas.height = this.minHeight;
-
-        ctx.save();
-        ctx.translate(
-            (this.canvas.width - (this.img.width * this.zoom)) / 2 + t.x,
-            (this.canvas.height - (this.img.height * this.zoom)) / 2 + t.y
-        );
-        ctx.scale(this.zoom, this.zoom);
-
-        ctx.drawImage(this.img, 0, 0);
-        ctx.restore();
-
-        if (typeof this.onChange === 'function') {
-            this.onChange();
-        }
-    }
-
-    setFile(file) {
-        const reader = new FileReader(),
-            img = document.createElement('img');
-
-        img.onload = function() {
-            this._translate.x = this._translate.y = 0;
-            this._offset.x = this._offset.y = 0;
-            this.zoom = 0;
-
-            // this is needed to keep jpegs from flashing
-            // on drag
-            this.img = document.createElement('canvas');
-            this.img.width = img.width;
-            this.img.height = img.height;
-            this.img.getContext('2d')
-                .drawImage(img, 0, 0);
-
-            this.hardZoom();
-
-            this.draw();
-        }.bind(this);
-
-        reader.onload = function(evt) {
-            img.src = evt.target.result;
-        }.bind(this);
-
-        reader.readAsDataURL(file);
-    }
-
-    hardTranslate() {
-        const halfImgHeight = (this.img.height * this.zoom) / 2,
-            halfCanvasHeight = this.minHeight / 2,
-            halfImgWidth = (this.img.width * this.zoom) / 2,
-            halfCanvasWidth = this.minWidth / 2;
-        let x = this._translate.x + this._offset.x,
-            y = this._translate.y + this._offset.y;
-
-        if (halfImgHeight - halfCanvasHeight < y) {
-            y = halfImgHeight - halfCanvasHeight;
-        }
-
-        if (halfCanvasHeight - halfImgHeight > y) {
-            y = halfCanvasHeight - halfImgHeight;
-        }
-
-        if (halfImgWidth - halfCanvasWidth < x) {
-            x = halfImgWidth - halfCanvasWidth;
-        }
-
-        if (halfCanvasWidth - halfImgWidth > x) {
-            x = halfCanvasWidth - halfImgWidth;
-        }
-
-        return { x: x, y: y };
-    }
-
-    hardZoom() {
-        let zoom;
-
-        if (!this.img) {
-            return;
-        }
-
-        if (!this.minWidth || !this.minHeight) {
-            return;
-        }
-
-        if (
-            this.img.width / this.img.height >
-            this.minWidth / this.minHeight
-        ) {
-            zoom = this.minHeight / this.img.height;
-        } else {
-            zoom = this.minWidth / this.img.width;
-        }
-
-        if (this.zoom < zoom) {
-            this.zoom = zoom;
-        }
-    }
-
-    translate(point) {
-        if (!this.img) {
-            return;
-        }
-
-        if (!this.minWidth || !this.minHeight) {
-            return;
-        }
-
-        this._translate.x = point.x;
-        this._translate.y = point.y;
-
-        this.draw();
-    }
-
-    offset(point) {
-        if (!this.img) {
-            return;
-        }
-
-        if (!this.minWidth || !this.minHeight) {
-            return;
-        }
-
-        this._translate.x = 0;
-        this._translate.y = 0;
-        this._offset.x = point.x;
-        this._offset.y = point.y;
-        this._offset = this.hardTranslate();
-
-        this.draw();
-    }
-}
-
-class EventTravel {
-    x = 0;
-    y = 0;
-
-    constructor(evt) {
-        this.x = evt.pageX;
-        this.y = evt.pageY;
-    }
-
-    diff(evt) {
-        return {
-            x: evt.pageX - this.x,
-            y: evt.pageY - this.y
-        };
-    }
-
-    dist(evt) {
-        const travel = this.diff(evt);
-
-        return Math.sqrt(travel.x * travel.x + travel.y * travel.y);
-    }
+    require('./image-preview-upload.less');
 }
 
 class ImagePreviewUpload extends React.Component {
     state = {
-        isDragActive: false,
-        isDragReject: false,
-        width: 0,
-        height: 0,
-        loading: false
+        loading: false,
+        moving: false
     };
 
     constructor(props) {
         super(props);
 
-        this.image = new ImageHandler(this.updateImage.bind(this));
+        this.image = new ImageHandler();
+        this.image.change(this.updateImage.bind(this));
 
         this.boundMove = this.move.bind(this);
         this.boundEnd = this.end.bind(this);
@@ -237,7 +40,8 @@ class ImagePreviewUpload extends React.Component {
     updateImage() {
         if (this.state.loading) {
             this.setState({
-                loading: false
+                loading: false,
+                moving: true
             });
         }
     }
@@ -256,6 +60,12 @@ class ImagePreviewUpload extends React.Component {
     move(evt) {
         evt.preventDefault();
         this.image.translate(this.startEvt.diff(evt));
+
+        if (this.startEvt.dist(evt) > 5) {
+            this.setState({
+                moving: true
+            });
+        }
     }
 
     end(evt) {
@@ -263,7 +73,10 @@ class ImagePreviewUpload extends React.Component {
             this.fileInputEl.value = null;
             this.fileInputEl.click();
         } else {
-            this.image.offset(this.startEvt.diff(evt));
+            this.image.offset();
+            this.setState({
+                moving: false
+            });
         }
 
         this.startEvt = null;
@@ -274,67 +87,16 @@ class ImagePreviewUpload extends React.Component {
 
     onScroll(evt) {
         evt.preventDefault();
-    }
-
-    onDragEnter(evt) {
-        let files = [],
-            allFilesAccepted = false;
-
-        evt.preventDefault();
-
-        ++this.enterCounter;
-
-        if (evt.dataTransfer && evt.dataTransfer.items) {
-            files = Array.prototype.slice.call(files);
-        }
-
-        if (
-            files.length === 1 &&
-            files[0].type.match('image.*') &&
-            (
-                typeof this.props.accept !== 'function' ||
-                this.props.accept(files[0])
-            )
-        ) {
-            allFilesAccepted = true;
-        }
-
-        this.setState({
-            isDragActive: allFilesAccepted,
-            isDragReject: !allFilesAccepted
-        });
-    }
-
-    onDragOver(evt) {
-        evt.preventDefault();
         evt.stopPropagation();
-        return false;
-    }
 
-    onDragLeave(evt) {
-        evt.preventDefault();
-
-        if (--this.enterCounter > 0) {
-            return;
-        }
-
-        this.setState({
-            isDragActive: false,
-            isDragReject: false
-        });
+        this.image.zoom(evt.deltaY * 0.04);
     }
 
     onDrop(evt) {
         let files;
 
-        evt.preventDefault();
-
-        this.enterCounter = 0;
-
         this.setState({
-            isDragActive: false,
-            isDragReject: false,
-            loading: true
+            loading: false
         });
 
         files = evt.dataTransfer ? evt.dataTransfer.files : evt.target.files;
@@ -352,24 +114,44 @@ class ImagePreviewUpload extends React.Component {
         this.image.setFile(files[0]);
     }
 
+    componentWillUpdate(nextProps, nextState) {
+        if (nextState.moving !== this.state.moving) {
+            if (nextState.moving) {
+                velocity(this.image.canvas, {
+                    scale: 0.25
+                }, 200, [ 100, 12 ]);
+            } else {
+                velocity(this.image.canvas, {
+                    scale: 1
+                }, 200, [ 100, 12 ]);
+            }
+        }
+    }
+
     render() {
-        let loader;
+        let loader, preview;
 
         if (this.state.loading) {
             loader = <div className="loading">{ this.props.loader }</div>;
         }
 
+        if (this.state.moving) {
+            preview = (
+                <DragPreview
+                    image={ this.image }
+                    scale={ 0.25 } />
+                );
+        }
+
         return (
             <div className="image-preview-upload"
                 onMouseDown={ this.down.bind(this) }
-                onDragEnter={ this.onDragEnter.bind(this) }
-                onDragOver={ this.onDragOver.bind(this) }
-                onDragLeave={ this.onDragLeave.bind(this) }
-                onDrop={ this.onDrop.bind(this) }
-                onScroll={ this.onScroll.bind(this) } >
+                onWheel={ this.onScroll.bind(this) } >
+                <DropZone onDrop={ this.onDrop.bind(this) } />
                 <input type="file"
                     ref={ (el) => this.fileInputEl = el }
                     onChange={ this.onDrop.bind(this) } />
+                { preview }
                 { loader }
             </div>
         );
